ceefd19c17d85eac92d5ef8cf293534c50d3fec8
Rajat Raj <rajatrajemails@gmail.com>
2024-02-24 10:17:53 -0800

    Design modified

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,6 +1,7 @@
 import LoginForm from "@/components/loginForm/loginForm";
 import { handleGithubLogin } from "@/lib/actions";
 import styles from "./login.module.css";
+import Image from "next/image";
 
 export const metadata = {
   title: "Login page",
@@ -11,9 +12,28 @@ const Login = () => {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <form action={handleGithubLogin}>
-          <button className={styles.github}>Login with Github</button>
-        </form>
+        <div className={styles.socialLogin}>
+          <form className={styles.google}>
+            <button>
+              <Image
+                src="/google.png"
+                alt="Google image"
+                width={20}
+                height={20}
+              />
+            </button>
+          </form>
+          <form action={handleGithubLogin} className={styles.github}>
+            <button>
+              <Image
+                src="/github.png"
+                alt="Github image"
+                width={20}
+                height={20}
+              />
+            </button>
+          </form>
+        </div>
         <LoginForm />
       </div>
     </div>
